8ca306ecf6282091a9d2ec1c513edbff7eed6b43
general <general@general.local>
2019-11-04 20:17:49 +0100

    added a basic routing layout in app.tsx:

--- frontend/scribble/src/container/App/index.tsx
+++ frontend/scribble/src/container/App/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import * as React from 'react';
+import { Redirect, Switch } from 'react-router-dom';
 
-import { IonApp } from '@ionic/react';
+import {
+  IonApp,
+  IonContent,
+  IonPage,
+  IonReactRouter,
+  IonSlides,
+  IonSlide
+  } from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -9,7 +17,15 @@ import Home from '/srv/app/frontend/src/page/Home';
 
 const App = () => (
   <IonApp>
-    <Home />
+    <IonReactRouter>
+      <IonPage>
+        <Home />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/blog" component={Blog} />
+        </Switch>
+      </IonPage>
+    </IonReactRouter>
   </IonApp>
 );
 
